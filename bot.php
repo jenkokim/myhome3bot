111e78ea76563f44876f05cf44602fd1c30dc811
giovanni <gilivanni@hotmail.it>
2019-05-16 23:13:09 +0200

    prova2

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
-<?php
 
 
-include "execute.php";
+
+
 
--- execute.php
+++ execute.php
@@ -1,5 +1,5 @@
 <?php
-include "bot.php";
+
 define('token', "668920983:AAH5OAvntJoUGWEDjE9CKQ9P4nQdY5MybxI");
 
 define('api' , "https://api.telegram.org/bot" .token . "/");
